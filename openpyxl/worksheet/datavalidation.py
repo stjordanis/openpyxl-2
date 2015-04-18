b081ba5d6bcc8bfe76a22582774179eb0f2e8df9
Charlie Clark <charlie.clark@clark-consulting.eu>
2015-04-18 15:03:42 +0200

    Descriptors renamed for clarity.

--- openpyxl/worksheet/datavalidation.py
+++ openpyxl/worksheet/datavalidation.py
@@ -6,7 +6,7 @@ import warnings
 
 from openpyxl.descriptors.serialisable import Serialisable
 from openpyxl.descriptors import Bool, NoneSet, Set, String
-from openpyxl.descriptors.nested import Text
+from openpyxl.descriptors.nested import NestedText
 from openpyxl.compat import OrderedDict, safe_string, deprecated, basestring
 from openpyxl.cell import coordinate_from_string
 from openpyxl.worksheet import cells_from_range
@@ -79,8 +79,8 @@ class DataValidation(Serialisable):
     promptTitle = String(allow_none = True)
     prompt = String(allow_none = True)
     sqref = String(allow_none = True)
-    formula1 = Text(allow_none=True, expected_type=basestring)
-    formula2 = Text(allow_none=True, expected_type=basestring)
+    formula1 = NestedText(allow_none=True, expected_type=basestring)
+    formula2 = NestedText(allow_none=True, expected_type=basestring)
 
     type = NoneSet(values=("whole", "decimal", "list", "date", "time",
                            "textLength", "custom"))
