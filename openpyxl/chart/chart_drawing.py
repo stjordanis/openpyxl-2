1f6cb239329563a69a54232f00d717ea0a77017a
Charlie Clark <charlie.clark@clark-consulting.eu>
2015-05-07 09:13:26 +0200

    Replace object with nested descriptor.

--- openpyxl/chart/chart_drawing.py
+++ openpyxl/chart/chart_drawing.py
@@ -10,6 +10,7 @@ from openpyxl.descriptors import (
 )
 from openpyxl.descriptors.nested import (
     NestedText,
+    NestedNoneSet,
 )
 
 from openpyxl.packaging.relationship import Relationship
@@ -19,7 +20,6 @@ from openpyxl.xml.constants import SHEET_DRAWING_NS
 
 from .chartspace import RelId
 from .shapes import (
-    Shape,
     Point2D,
     PositiveSize2D,
 )
@@ -70,7 +70,7 @@ class AnchorMarker(Serialisable):
 class _AnchorBase(Serialisable):
 
     #one of
-    sp = Typed(expected_type=Shape, allow_none=True)
+    sp = NestedNoneSet(values=(['cone', 'coneToMax', 'box', 'cylinder', 'pyramid', 'pyramidToMax']))
     grpSp = Typed(expected_type=GroupShape, allow_none=True)
     graphicFrame = Typed(expected_type=GraphicFrame, allow_none=True)
     cxnSp = Typed(expected_type=Connector, allow_none=True)
