f3d249f66a7ee54ced0b9377bcd180845df0fea7
Charlie Clark <charlie.clark@clark-consulting.eu>
2015-05-21 16:39:57 +0200

    Remove unused module.

--- openpyxl/chart/drawing.py
+++
@@ -1,9 +0,0 @@
-from __future__ import absolute_import
-# Copyright (c) 2010-2015 openpyxl
-
-from openpyxl.descriptors.serialisable import Serialisable
-
-
-class OfficeArtExtensionList(Serialisable):
-
-    pass
