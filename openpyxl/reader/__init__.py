30299d38dcaa783d159b7e4e00506a3fc7592cf0
Charlie Clark <charlie.clark@clark-consulting.eu>
2015-10-05 20:17:05 +0200

    Parsing code now replaced by more deterministic Stylesheet class.

--- openpyxl/reader/__init__.py
+++ openpyxl/reader/__init__.py
@@ -6,6 +6,5 @@ from __future__ import absolute_import
 # package imports
 from openpyxl.reader import excel
 from openpyxl.reader import strings
-from openpyxl.reader import style
 from openpyxl.reader import workbook
 from openpyxl.reader import worksheet
