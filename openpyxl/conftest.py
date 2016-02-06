a9e922977b1bc1a696769f8aa8ae7bc7a0c631e6
Charlie Clark <charlie.clark@clark-consulting.eu>
2016-02-06 17:35:13 +0100

    Remove unused fixture.

--- openpyxl/conftest.py
+++ openpyxl/conftest.py
@@ -18,12 +18,6 @@ def Worksheet():
 
 # Global fixtures
 
-@pytest.fixture
-def root_xml():
-    """Root XML element <test>"""
-    from openpyxl.xml.functions import Element
-    return Element("test")
-
 
 ### Markers ###
 
