67b20e1ad524079f71abc0c22d56702905789317
Charlie Clark <charlie.clark@clark-consulting.eu>
2016-03-13 11:14:06 +0100

    Whitespace

--- openpyxl/workbook/names/external.py
+++ openpyxl/workbook/names/external.py
@@ -87,8 +87,6 @@ def parse_ranges(xml):
         yield ExternalRange(**n.attrib)
 
 
-
-
 def detect_external_links(rels, archive):
     from openpyxl.reader.workbook import find_external_refs
     rels = dict(rels)
