6d5c4ab146bc31567861d2d83982c00c3895a604
Charlie Clark <charlie.clark@clark-consulting.eu>
2017-05-15 12:00:11 +0200

    Move PivotCache object to parser module.

--- openpyxl/writer/workbook.py
+++ openpyxl/writer/workbook.py
@@ -16,13 +16,12 @@ from openpyxl.xml.constants import (
 )
 from openpyxl.xml.functions import tostring, fromstring
 
-from openpyxl.workbook.pivot import PivotCache
 from openpyxl.worksheet import Worksheet
 from openpyxl.chartsheet import Chartsheet
 from openpyxl.packaging.relationship import Relationship, RelationshipList
 from openpyxl.workbook.defined_name import DefinedName
 from openpyxl.workbook.external_reference import ExternalReference
-from openpyxl.workbook.parser import ChildSheet, WorkbookPackage
+from openpyxl.workbook.parser import ChildSheet, WorkbookPackage, PivotCache
 from openpyxl.workbook.properties import CalcProperties, WorkbookProperties
 from openpyxl.workbook.views import BookView
 from openpyxl.utils.datetime import CALENDAR_MAC_1904
