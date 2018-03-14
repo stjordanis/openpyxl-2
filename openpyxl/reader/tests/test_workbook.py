0c7542ddb0ab5167534dab2e37ead5d1272e14d5
Charlie Clark <charlie.clark@clark-consulting.eu>
2018-03-14 15:51:31 +0100

    Move workbook reading code to reader module to avoid circular imports.

--- openpyxl/reader/tests/test_workbook.py
+++ openpyxl/reader/tests/test_workbook.py
@@ -75,7 +75,7 @@ class TestWorkbookParser:
 
 
     def test_broken_sheet_ref(self, datadir, recwarn, WorkbookParser):
-        from openpyxl.workbook.parser import WorkbookPackage
+        from openpyxl.packaging.workbook import WorkbookPackage
         datadir.chdir()
         with open("workbook_missing_id.xml", "rb") as src:
             xml = src.read()
