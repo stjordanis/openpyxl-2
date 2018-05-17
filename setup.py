01501198772f43a31f7207d791b0c9403aa42aa1
Charlie Clark <charlie.clark@clark-consulting.eu>
2018-05-17 12:08:51 +0200

    Remove traces of JSON.

--- setup.py
+++ setup.py
@@ -57,9 +57,6 @@ setup(name='openpyxl',
     install_requires=[
         'jdcal', 'et_xmlfile',
         ],
-    package_data={
-        'openpyxl': ['.constants.json']
-    },
     classifiers=[
                  'Development Status :: 5 - Production/Stable',
                  'Operating System :: MacOS :: MacOS X',
