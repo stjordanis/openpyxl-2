ce604b4daa1cde0a22d671a191e57a4158a41ebf
Charlie Clark <charlie.clark@clark-consulting.eu>
2018-07-11 11:37:34 +0200

    Move ChartRelation to a separate module.

--- openpyxl/drawing/graphic.py
+++ openpyxl/drawing/graphic.py
@@ -14,7 +14,6 @@ from openpyxl.descriptors import (
     String,
     Alias,
 )
-from openpyxl.descriptors.excel import Relation
 from openpyxl.descriptors.excel import ExtensionList as OfficeArtExtensionList
 
 from openpyxl.chart.shapes import GraphicalProperties
@@ -28,6 +27,7 @@ from .geometry import (
     ShapeStyle,
     GroupTransform2D
 )
+from .relation import ChartRelation
 from .xdr import XDRTransform2D
 
 
@@ -394,17 +394,6 @@ class NonVisualGraphicFrame(Serialisable):
         if cNvGraphicFramePr is None:
             cNvGraphicFramePr = NonVisualGraphicFrameProperties()
         self.cNvGraphicFramePr = cNvGraphicFramePr
-
-
-class ChartRelation(Serialisable):
-
-    tagname = "chart"
-    namespace = CHART_NS
-
-    id = Relation()
-
-    def __init__(self, id):
-        self.id = id
 
 
 class GraphicData(Serialisable):
