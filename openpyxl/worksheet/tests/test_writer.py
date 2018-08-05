8d5595357aa94eb8dab63c1bb82321c631567648
Update ignore <Update ignore@localhost>
2018-08-05 19:33:45 +0200

    Combine context manager and generator for more isolated testing.

--- openpyxl/worksheet/tests/test_writer.py
+++ openpyxl/worksheet/tests/test_writer.py
@@ -3,3 +3,34 @@ from __future__ import absolute_import
 
 import pytest
 from openpyxl.xml.functions import fromstring, tostring
+from openpyxl.tests.helper import compare_xml
+
+from openpyxl.workbook import Workbook
+
+
+@pytest.fixture
+def WorksheetWriter():
+    from ..writer import WorksheetWriter
+    return WorksheetWriter
+
+
+class TestWorksheetWriter:
+
+
+    def test_properties(self, WorksheetWriter):
+        wb = Workbook()
+        ws = wb.active
+        writer = WorksheetWriter(ws)
+        writer.write_properties()
+        writer.xf.close()
+        xml = writer.out.getvalue()
+        expected = """
+        <worksheet xmlns="http://schemas.openxmlformats.org/spreadsheetml/2006/main">
+           <sheetPr>
+             <outlinePr summaryRight="1" summaryBelow="1"/>
+             <pageSetUpPr/>
+        </sheetPr>
+        </worksheet>
+        """
+        diff = compare_xml(xml, expected)
+        assert diff is None, diff
