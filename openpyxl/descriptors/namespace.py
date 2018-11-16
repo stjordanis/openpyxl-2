887cac5d36f3a5d80c65609e06959c9c7888a6fb
Charlie Clark <charlie.clark@clark-consulting.eu>
2018-11-16 17:38:04 +0100

    Allow namespaces to be set on descriptors.

--- openpyxl/descriptors/namespace.py
+++ openpyxl/descriptors/namespace.py
@@ -7,7 +7,7 @@ def namespaced(obj, tagname, namespace=None):
     Utility to create a namespaced tag for an object
     """
 
-    namespace = getattr(obj, "namespace", namespace)
+    namespace = getattr(obj, "namespace", None) or namespace
     if namespace is not None:
         tagname = "{%s}%s" % (namespace, tagname)
     return tagname
