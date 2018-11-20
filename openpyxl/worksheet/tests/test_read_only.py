f634408580e56c043ab1699411ee3a5d1239ce16
Charlie Clark <charlie.clark@clark-consulting.eu>
2018-11-20 16:46:08 +0100

    Make more use of the fixture.

--- openpyxl/worksheet/tests/test_read_only.py
+++ openpyxl/worksheet/tests/test_read_only.py
@@ -25,19 +25,21 @@ def DummyWorkbook():
 
 
 @pytest.fixture
-def ReadOnlyWorksheet():
+def ReadOnlyWorksheet(DummyWorkbook, datadir):
     from ..read_only import ReadOnlyWorksheet
-    return ReadOnlyWorksheet
+    datadir.chdir()
+
+    wb = DummyWorkbook
+    wb._archive.write("sheet_inline_strings.xml", "sheet1.xml")
+    ws = ReadOnlyWorksheet(wb, "Sheet", "sheet1.xml", [])
+
+    return ws
 
 
 class TestReadOnlyWorksheet:
 
-    def test_from_xml(self, datadir, ReadOnlyWorksheet, DummyWorkbook):
-        datadir.chdir()
-        wb = DummyWorkbook
-        wb._archive.write("sheet_inline_strings.xml", "sheet1.xml")
-
-        ws = ReadOnlyWorksheet(wb, "Sheet", "sheet1.xml", [])
+    def test_from_xml(self, ReadOnlyWorksheet):
+        ws = ReadOnlyWorksheet
         cells = tuple(ws.iter_rows(min_row=1, min_col=1, max_row=1, max_col=1))
         assert len(cells) == 1
         assert cells[0][0].value == "col1"
@@ -61,49 +63,48 @@ class TestReadOnlyWorksheet:
 
         wb = DummyWorkbook
         wb._archive.writestr("sheet1.xml", src)
-        ws = ReadOnlyWorksheet(wb, "Sheet", "sheet1.xml", [])
+        ws = ReadOnlyWorksheet
         ws._xml = BytesIO(src)
         cell = ws._get_cell(row, column)
         assert cell is EMPTY_CELL
 
 
-    def test_pad_row_left(self, ReadOnlyWorksheet, DummyWorkbook):
+    def test_pad_row_left(self, ReadOnlyWorksheet):
         row = [
             {'column':4, 'value':4,},
             {'column':8, 'value':8,},
         ]
-        ws = ReadOnlyWorksheet(DummyWorkbook, "Sheet", "", [])
+        ws = ReadOnlyWorksheet
         cells = ws._get_row(row, max_col=4, values_only=True)
         assert cells == (None, None, None, 4)
 
 
-    def test_pad_row(self, ReadOnlyWorksheet, DummyWorkbook):
+    def test_pad_row(self, ReadOnlyWorksheet):
         row = [
             {'column':4, 'value':4,},
             {'column':8, 'value':8,},
         ]
-        ws = ReadOnlyWorksheet(DummyWorkbook, "Sheet", "", [])
+        ws = ReadOnlyWorksheet
         cells = ws._get_row(row, min_col=4, max_col=8, values_only=True)
         assert cells == (4, None, None, None, 8)
 
 
-    def test_pad_row_right(self, ReadOnlyWorksheet, DummyWorkbook):
+    def test_pad_row_right(self, ReadOnlyWorksheet):
         row = [
             {'column':4, 'value':4},
             {'column':8, 'value':8},
         ]
-        ws = ReadOnlyWorksheet(DummyWorkbook, "Sheet", "", [])
+        ws = ReadOnlyWorksheet
         cells = ws._get_row(row, min_col=6, max_col=10, values_only=True)
         assert cells == (None, None, 8, None, None)
 
 
-    def test_pad_row_cells(self, ReadOnlyWorksheet, DummyWorkbook):
-        wb = DummyWorkbook
+    def test_pad_row_cells(self, ReadOnlyWorksheet):
         row = [
             {'column':4, 'value':4, 'row':2},
             {'column':8, 'value':8, 'row':2},
         ]
-        ws = ReadOnlyWorksheet(DummyWorkbook, "Sheet", "", [])
+        ws = ReadOnlyWorksheet
         cells = ws._get_row(row, min_col=6, max_col=10)
         assert cells == (
             EMPTY_CELL, EMPTY_CELL,
@@ -112,21 +113,15 @@ class TestReadOnlyWorksheet:
         )
 
 
-    def test_read_rows(self, ReadOnlyWorksheet, DummyWorkbook):
-        wb = DummyWorkbook
-        wb._archive.write("sheet_inline_strings.xml", "sheet1.xml")
-
-        ws = ReadOnlyWorksheet(DummyWorkbook, "Sheet", "sheet1.xml", [])
+    def test_read_rows(self, ReadOnlyWorksheet):
+        ws = ReadOnlyWorksheet
         rows = ws._cells_by_row(min_row=1, max_row=1, min_col=1, max_col=3, values_only=True)
         row = next(rows)
         assert row == ('col1', 'col2', 'col3')
 
 
-    def test_pad_rows_before(self, ReadOnlyWorksheet, DummyWorkbook):
-        wb = DummyWorkbook
-        wb._archive.write("sheet_inline_strings.xml", "sheet1.xml")
-
-        ws = ReadOnlyWorksheet(DummyWorkbook, "Sheet", "sheet1.xml", [])
+    def test_pad_rows_before(self, ReadOnlyWorksheet):
+        ws = ReadOnlyWorksheet
         rows = ws._cells_by_row(min_row=8, max_row=10, min_col=1, max_col=3, values_only=True)
         assert list(rows) == [
             (None, None, None),
@@ -135,11 +130,8 @@ class TestReadOnlyWorksheet:
         ]
 
 
-    def test_pad_rows_after(self, ReadOnlyWorksheet, DummyWorkbook):
-        wb = DummyWorkbook
-        wb._archive.write("sheet_inline_strings.xml", "sheet1.xml")
-
-        ws = ReadOnlyWorksheet(DummyWorkbook, "Sheet", "sheet1.xml", [])
+    def test_pad_rows_after(self, ReadOnlyWorksheet):
+        ws = ReadOnlyWorksheet
         rows = ws._cells_by_row(min_row=4, max_row=6, min_col=1, max_col=3, values_only=True)
         assert list(rows) == [
             (7, 8, 9),
@@ -148,11 +140,8 @@ class TestReadOnlyWorksheet:
         ]
 
 
-    def test_pad_rows_between(self, ReadOnlyWorksheet, DummyWorkbook):
-        wb = DummyWorkbook
-        wb._archive.write("sheet_inline_strings.xml", "sheet1.xml")
-
-        ws = ReadOnlyWorksheet(DummyWorkbook, "Sheet", "sheet1.xml", [])
+    def test_pad_rows_between(self, ReadOnlyWorksheet):
+        ws = ReadOnlyWorksheet
         rows = ws._cells_by_row(min_row=4, max_row=None, min_col=1, max_col=3, values_only=True)
         assert list(rows) == [
             (7, 8, 9),
@@ -165,11 +154,8 @@ class TestReadOnlyWorksheet:
         ]
 
 
-    def test_pad_rows_bounded(self, ReadOnlyWorksheet, DummyWorkbook):
-        wb = DummyWorkbook
-        wb._archive.write("sheet_inline_strings.xml", "sheet1.xml")
-
-        ws = ReadOnlyWorksheet(DummyWorkbook, "Sheet", "sheet1.xml", [])
+    def test_pad_rows_bounded(self, ReadOnlyWorksheet):
+        ws = ReadOnlyWorksheet
         rows = ws._cells_by_row(min_row=8, max_row=15, min_col=1, max_col=3, values_only=True)
         assert list(rows) == [
             (None, None, None),
