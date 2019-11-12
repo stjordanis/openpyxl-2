7b2bf5159b924621d11c6e87008821d8704e074b
Charlie Clark <charlie.clark@clark-consulting.eu>
2019-11-12 16:31:48 +0100

    Use a set comprehension.

--- openpyxl/worksheet/_reader.py
+++ openpyxl/worksheet/_reader.py
@@ -261,11 +261,8 @@ class WorkSheetParser(object):
             self.max_row = int(attrs['r'])
         else:
             self.max_row += 1
-        keys = set(attrs)
-        for key in keys:
-            if key.startswith('{'):
-                del attrs[key]
 
+        keys = {k for k in attrs if not k.startswith('{')}
         if keys != {'r', 'spans'} and keys != {'r'}:
             # don't create dimension objects unless they have relevant information
             self.row_dimensions[attrs['r']] = attrs
