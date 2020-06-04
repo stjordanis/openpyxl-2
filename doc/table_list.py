56eb4fa8f377244ce2bd1b62f84e4d11ecb2c0c9
saykharng <saykharng@localhost>
2020-06-04 20:55:55 +1000

    attempting two fix merge conflict

--- doc/table_list.py
+++
@@ -1,5 +0,0 @@
-#Get table by nmae 
-ws['Table1'] #Returns a table object with that name
-wb.tables.get("Table1") #Return a table object with that name
-wb.tables.delete("Table1") #Delete a table object with that name
-wb.tables.items() #Returns a tuple with (table name, table ref)
