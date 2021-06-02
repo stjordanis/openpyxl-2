2a219fd098c9e7a82f3d1deb538d75de9a709474
Charlie Clark <charlie.clark@clark-consulting.eu>
2021-06-02 17:57:42 +0200

    Simpler implementation of custom properties. Adjust tests.

--- openpyxl/packaging/tests/test_custom.py
+++ openpyxl/packaging/tests/test_custom.py
@@ -15,19 +15,22 @@ def CustomDocumentProperty():
 class TestCustomDocumentProperty:
 
     def test_ctor(self, CustomDocumentProperty):
-        prop = CustomDocumentProperty("PropName9", True)
+        prop = CustomDocumentProperty(name="PropName9", value=True)
+        assert prop.type == "bool"
+        assert prop.bool is True
         expected = """
-        <property name="PropName9" pid="None" fmtid="{D5CDD505-2E9C-101B-9397-08002B2CF9AE}">
-          <vt:bool xmlns:vt="http://schemas.openxmlformats.org/officeDocument/2006/docPropsVTypes">true</vt:bool>
+        <property name="PropName9" pid="0" fmtid="{D5CDD505-2E9C-101B-9397-08002B2CF9AE}">
+          <vt:bool xmlns:vt="http://schemas.openxmlformats.org/officeDocument/2006/docPropsVTypes">1</vt:bool>
         </property>
         """
         xml = tostring(prop.to_tree())
         diff = compare_xml(xml, expected)
         assert diff is None, diff
 
+
     def test_from_xml(self, CustomDocumentProperty):
         src = """
-        <property name="PropName1" pid="None" fmtid="{D5CDD505-2E9C-101B-9397-08002B2CF9AE}">
+        <property name="PropName1" pid="0" fmtid="{D5CDD505-2E9C-101B-9397-08002B2CF9AE}">
           <vt:filetime xmlns:vt="http://schemas.openxmlformats.org/officeDocument/2006/docPropsVTypes">2020-08-24T20:19:22Z</vt:filetime>
         </property>
         """
@@ -36,7 +39,7 @@ class TestCustomDocumentProperty:
         assert prop.value == datetime.datetime(2020, 8, 24, hour=20, minute=19, second=22) and prop.name == "PropName1"
 
         src = """
-        <property name="PropName4" pid="None" fmtid="{D5CDD505-2E9C-101B-9397-08002B2CF9AE}" linkTarget="ExampleName">
+        <property name="PropName4" pid="0" fmtid="{D5CDD505-2E9C-101B-9397-08002B2CF9AE}" linkTarget="ExampleName">
           <vt:lpwstr xmlns:vt="http://schemas.openxmlformats.org/officeDocument/2006/docPropsVTypes"/>
         </property>
         """
@@ -82,11 +85,12 @@ class TestCustomDocumentProperyList:
 
         assert len(props) == 0
 
+
     def test_ctor(self, CustomDocumentPropertyList):
         props = CustomDocumentPropertyList()
-        props.add("PropName1", "2020-08-24T20:19:22Z", "date")
+        props.add("PropName1", datetime.datetime(2020, 8, 24, 20, 19, 22))
         props.add("PropName2", LinkTarget="ExampleName")
-        props.add("PropName3", "2.5", float)
+        props.add("PropName3", 2.5)
 
         xml = tostring(props.to_tree())
         expected = """
@@ -106,7 +110,7 @@ class TestCustomDocumentProperyList:
         assert diff is None, diff
 
 
-    def test_from_xml(self, CustomDocumentPropertyList):
+    def test_from_xml(self, CustomDocumentPropertyList, CustomDocumentProperty):
         src = """
         <Properties xmlns:vt="http://schemas.openxmlformats.org/officeDocument/2006/docPropsVTypes" xmlns="http://schemas.openxmlformats.org/officeDocument/2006/custom-properties">
           <property name="PropName1" pid="2" fmtid="{D5CDD505-2E9C-101B-9397-08002B2CF9AE}">
@@ -123,33 +127,8 @@ class TestCustomDocumentProperyList:
         node = fromstring(src)
         props = CustomDocumentPropertyList.from_tree(node)
 
-        props2 = CustomDocumentPropertyList()
-        props2.add("PropName1", datetime.datetime(2020, 8, 24, hour=20, minute=19, second=22))
-        props2.add("PropName2", 2.5)
-        props2.add("PropName3", True)
-
-        for prop in props:
-            prop2 = props2[prop.name]
-            assert prop.value == prop2.value
-
-        xml = tostring(props.to_tree())
-        diff = compare_xml(xml, src)
-        assert diff is None, diff
-
-
-# # don't use datetime.now() in tests, it will fail
-# customDocumentProperty1 = CustomDocumentProperty("PropName1", datetime.datetime.now().isoformat(timespec='seconds'), 'date')
-# # the following should work, if further tests are needed:
-# customDocumentProperty1 = CustomDocumentProperty("PropName1", datetime.datetime(2020, 8, 24, hour=20, minute=19, second=22))
-# customDocumentProperty2 = CustomDocumentProperty("PropName2", "2020-08-24T20:19:22Z", "date")
-# customDocumentProperty3 = CustomDocumentProperty("PropName3", "2020-08-24T20:19:22Z", 'date')
-# customDocumentProperty4 = CustomDocumentProperty("PropName4", LinkTarget="ExampleName")
-# customDocumentProperty5 = CustomDocumentProperty("PropName5", 2.5)
-# customDocumentProperty6 = CustomDocumentProperty("PropName6", 2)
-# customDocumentProperty7 = CustomDocumentProperty("PropName7", "2.5", float)
-# customDocumentProperty8 = CustomDocumentProperty("PropName8", "Foo")
-# customDocumentProperty9 = CustomDocumentProperty("PropName9", True)
-# customDocumentProperty10 = CustomDocumentProperty("PropName10", False)
-# print(tostring(customDocumentProperty9.to_tree()))
-# val = customDocumentProperty9.value
-# print(str(val) + " type: " + str(type(val)))
+        assert props.customProps == [
+            CustomDocumentProperty(name="PropName1", value=datetime.datetime(2020, 8, 24, 20, 19, 22), pid=2),
+            CustomDocumentProperty(name="PropName2", value=2.5, pid=3),
+            CustomDocumentProperty(name="PropName3", value=True, pid=4),
+        ]
